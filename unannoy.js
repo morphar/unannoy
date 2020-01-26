143b105da7e74ff76ae12fcda10314c69ec20fc2
morphar <dan@techba.se>
2020-01-26 01:29:53 +0100

    Encapsulate the javascript

--- unannoy.js
+++ unannoy.js
@@ -1,103 +1,104 @@
-function removeClass(className) {
-    let elms = document.getElementsByClassName(className);
+(function() {
+    function removeClass(className) {
+        let elms = document.getElementsByClassName(className);
 
-    let re = new RegExp(className, 'gi');
+        let re = new RegExp(className, 'gi');
 
-    for (let i = elms.length - 1 ; i >= 0 ; i--) {
-        let elm = elms[i];
-        elm.className = elm.className.replace(re, '');
+        for (let i = elms.length - 1; i >= 0; i--) {
+            let elm = elms[i];
+            elm.className = elm.className.replace(re, '');
+        }
     }
-}
 
-function removeClassByQuery(query, className) {
-    let re = new RegExp(className, 'gi');
-    let elms = document.querySelectorAll(query)
-    for (let i = elms.length - 1 ; i >= 0 ; i--) {
-        let elm = elms[i];
-        elm.className = elm.className.replace(re, '');
+    function removeClassByQuery(query, className) {
+        let re = new RegExp(className, 'gi');
+        let elms = document.querySelectorAll(query)
+        for (let i = elms.length - 1; i >= 0; i--) {
+            let elm = elms[i];
+            elm.className = elm.className.replace(re, '');
+        }
     }
-}
 
-function removeById(id) {
-    removeElement(document.getElementById(id));
-}
-
-function removeElement(node) {
-    if (node && typeof node !== 'undefined' && typeof node.parentNode !== 'undefined') {
-        node.parentNode.removeChild(node);
+    function removeById(id) {
+        removeElement(document.getElementById(id));
     }
-}
 
-function removeByClass(className) {
-    let elms = document.getElementsByClassName(className);
-    for (let i = elms.length - 1 ; i >= 0 ; i--) {
-        removeElement(elms[i]);
+    function removeElement(node) {
+        if (node && typeof node !== 'undefined' && typeof node.parentNode !== 'undefined') {
+            node.parentNode.removeChild(node);
+        }
     }
-}
 
-function removeByAttribute(tag, attrName, attrValue) {
-    let elms = document.querySelectorAll(`${tag}[${attrName}='${attrValue}']`)
-    for (let i = elms.length - 1 ; i >= 0 ; i--) {
-        removeElement(elms[i]);
+    function removeByClass(className) {
+        let elms = document.getElementsByClassName(className);
+        for (let i = elms.length - 1; i >= 0; i--) {
+            removeElement(elms[i]);
+        }
     }
-}
 
-// Doesn't work in Safari
-function removeOverlays() {
-    let elms = document.body.childNodes;
+    function removeByAttribute(tag, attrName, attrValue) {
+        let elms = document.querySelectorAll(`${tag}[${attrName}='${attrValue}']`)
+        for (let i = elms.length - 1; i >= 0; i--) {
+            removeElement(elms[i]);
+        }
+    }
 
-    let viewportWidth = window.innerWidth;
-    let viewportHeight = window.innerHeight;
+    // Doesn't work in Safari
+    function removeOverlays() {
+        let elms = document.body.childNodes;
 
-    for (let i = elms.length - 1 ; i >= 0 ; i--) {
-        if (elms[i] instanceof Element) {
-            if (elms[i].className.indexOf('overlay') !== -1) {
-                removeElement(elms[i]);
-                continue;
-            }
+        let viewportWidth = window.innerWidth;
+        let viewportHeight = window.innerHeight;
 
-            let width = elms[i].offsetWidth;
-            let height = elms[i].offsetHeight;
-            if (width == viewportWidth && height == viewportHeight) {
-                removeElement(elms[i]);
+        for (let i = elms.length - 1; i >= 0; i--) {
+            if (elms[i] instanceof Element) {
+                if (elms[i].className.indexOf('overlay') !== -1) {
+                    removeElement(elms[i]);
+                    continue;
+                }
+
+                let width = elms[i].offsetWidth;
+                let height = elms[i].offsetHeight;
+                if (width == viewportWidth && height == viewportHeight) {
+                    removeElement(elms[i]);
+                }
             }
         }
     }
-}
 
-// Unset hidden overflows
-document.getElementsByTagName('html')[0].style.overflow = 'unset';
-document.body.style.overflow = 'unset';
+    // Unset hidden overflows
+    document.getElementsByTagName('html')[0].style.overflow = 'unset';
+    document.body.style.overflow = 'unset';
 
-// Google funding "choice"
-removeByClass('fc-ab-root');
-removeByAttribute('iframe', 'name', 'googlefcInactive');
-removeByAttribute('iframe', 'name', 'googlefcPresent');
+    // Google funding "choice"
+    removeByClass('fc-ab-root');
+    removeByAttribute('iframe', 'name', 'googlefcInactive');
+    removeByAttribute('iframe', 'name', 'googlefcPresent');
 
-// QuantCast
-removeClass('qc-cmp-showing');
-removeClass('qc-cmp-ui-showing');
-removeByClass('qc-cmp-ui-container');
+    // QuantCast
+    removeClass('qc-cmp-showing');
+    removeClass('qc-cmp-ui-showing');
+    removeByClass('qc-cmp-ui-container');
 
-// Medium
-removeByClass('overlay');
-removeClass('u-overflowHidden');
-// Privacy Policy bar
-removeByClass('butterBar--privacy');
+    // Medium
+    removeByClass('overlay');
+    removeClass('u-overflowHidden');
+    // Privacy Policy bar
+    removeByClass('butterBar--privacy');
 
-// CybotCookieDialog
-// removeByAttribute('div','role','alertdialog');
-removeById('CybotCookiebotDialog');
-removeById('CybotCookiebotDialogBodyUnderlay');
+    // CybotCookieDialog
+    // removeByAttribute('div','role','alertdialog');
+    removeById('CybotCookiebotDialog');
+    removeById('CybotCookiebotDialogBodyUnderlay');
 
-// Oil layer: https://www.oiljs.org/
-removeByClass('as-oil');
-removeClass('as-oil-js-active');
+    // Oil layer: https://www.oiljs.org/
+    removeByClass('as-oil');
+    removeClass('as-oil-js-active');
 
-// Generics
-removeClassByQuery('html', 'locked')
-removeClassByQuery('body', 'locked')
+    // Generics
+    removeClassByQuery('html', 'locked')
+    removeClassByQuery('body', 'locked')
 
-// Remove overlays
-removeOverlays();
-
+    // Remove overlays
+    removeOverlays();
+}())
